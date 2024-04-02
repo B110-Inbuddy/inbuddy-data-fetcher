b3a426034fca3f71adfc0b2577d34589690b8825
임서정 <sjytis14@naver.com>
2024-04-02 14:51:09 +0900

    [Feat] csv_merger의 column 영어로 변경, index 추가

--- flight-data/csv_merger.py
+++ flight-data/csv_merger.py
@@ -13,13 +13,19 @@ print('*' * 50)
 print('import path: ', path)
 print('import files: ', files)
 
+# COLUMNS = {
+#     'D': ["날짜", "항공사", "편명", "도착지", "계획", "예상", "실제", "구분", "현황", "사유"],
+#     'A': ["날짜", "항공사", "편명", "출발지", "계획", "예상", "실제", "구분", "현황", "사유"]
+# }
+
 COLUMNS = {
-    'D': ["날짜", "항공사", "편명", "출발지", "계획", "예상", "실제", "구분", "현황", "사유"],
-    'A': ["날짜", "항공사", "편명", "도착지", "계획", "예상", "실제", "구분", "현황", "사유"]
+    'D': ["departure_date", "airline", "flight_code", "destination", "departure_time_plan", "departure_time_expexted", "departure_time_real", "division", "flight_status", "cause"],
+    'A': ["arrival_date", "airline", "flight_code", "origin", "arrival_time_plan", "arrival_time_expexted", "arrival_time_real", "division", "flight_status", "cause"]
 }
 
-cat_arrive = pd.DataFrame(columns=[i for i in range(9)])
-cat_departure = pd.DataFrame(columns=[i for i in range(9)])
+
+cat_arrive = pd.DataFrame(columns=COLUMNS['A'])
+cat_departure = pd.DataFrame(columns=COLUMNS['D'])
 
 ### 디렉터리 생성
 print("*" * 50)
@@ -46,10 +52,12 @@ for file in files:
         print("*** Filename of " + file + " is incorrect. skip")
         continue
 
-    df = pd.read_csv(path + '/' + file, encoding='cp949', header=None, skiprows=1)
+    file_type = file[8]  # 'A' 또는 'D'
+    df = pd.read_csv(path + '/' + file, encoding='cp949', header=None, skiprows=1, names=COLUMNS[file_type])
 
     # df['날짜'] = pd.to_datetime(df['날짜'], format='%Y%m%d')
-
+    df = df[df['division'] != '화물']
+    
     START_DATE = min(START_DATE, file[:8])
     END_DATE = max(END_DATE, file[:8])
 
@@ -70,11 +78,26 @@ cat_departure.columns = COLUMNS['D']
 print("*" * 50)
 print("sort by date, time")
 
-cat_arrive.sort_values(by=['날짜', '계획'], ascending=[True, True], inplace=True)
-cat_departure.sort_values(by=['날짜', '계획'], ascending=[True, True], inplace=True)
 
-cat_arrive = cat_arrive.drop_duplicates().reset_index(drop=True)
-cat_departure = cat_departure.drop_duplicates().reset_index(drop=True)
+# cat_arrive.sort_values(by=['날짜', '계획'], ascending=[True, True], inplace=True)
+# cat_departure.sort_values(by=['날짜', '계획'], ascending=[True, True], inplace=True)
+
+cat_arrive.sort_values(by=['arrival_date', 'arrival_time_plan'], ascending=[True, True], inplace=True)
+cat_departure.sort_values(by=['departure_date', 'departure_time_plan'], ascending=[True, True], inplace=True)
+
+
+# cat_arrive = cat_arrive.drop_duplicates().reset_index(drop=True)
+# cat_departure = cat_departure.drop_duplicates().reset_index(drop=True)
+
+cat_arrive.reset_index(inplace=True)
+cat_departure.reset_index(inplace=True)
+
+cat_arrive['index'] = cat_arrive['index'] + 1
+cat_departure['index'] = cat_departure['index'] + 1
+
+
+cat_arrive.rename(columns={'index': 'flight_info_id'}, inplace=True)
+cat_departure.rename(columns={'index': 'flight_info_id'}, inplace=True)
 
 
 def filename(dep_arr):
--- weather-data/csv_merger.py
+++ weather-data/csv_merger.py
@@ -13,7 +13,9 @@ print('*' * 50)
 print('import path: ', path)
 print('import files: ', files)
 
-COLUMNS = ["지점", "일시", "기온(°C)", "10분평균풍속(KT)", "10분평균풍향(deg)", "10분평균MOR(m)", "10분평균RVR(m)", "누적강수량(mm)"]
+# COLUMNS = ["지점", "일시", "기온(°C)", "10분평균풍속(KT)", "10분평균풍향(deg)", "10분평균MOR(m)", "10분평균RVR(m)", "누적강수량(mm)"]
+COLUMNS = ["point", "weather_date", "temperature", "wind_speed_10m_avg_kt", "wind_dir_10m_avg_deg", "mor_10m_avg_m",
+           "rvr_10m_avg_m", " cumulative_precipitation_mm"]
 
 cat = pd.DataFrame(columns=[i for i in range(8)])
 
@@ -31,7 +33,7 @@ print("start concatenation")
 
 for file in files:
     print("* " + file)
-    
+
     if not file.endswith(".csv"):
         print("*** " + file + " is not an csv file. skip")
         continue
@@ -39,7 +41,6 @@ for file in files:
     df = pd.read_csv(path + '/' + file, encoding='cp949', header=None, skiprows=1)
 
     # df['일시'] = pd.to_datetime(df['일시'], format='%Y%m%d')
-
     cat = pd.concat([cat, df], ignore_index=True)
 
 print("concatenation completed")
@@ -53,17 +54,27 @@ cat.columns = COLUMNS
 print("*" * 50)
 print("sort by date, time")
 
-cat.sort_values(by=['일시'], ascending=True, inplace=True)
+# cat.sort_values(by=['일시'], ascending=True, inplace=True)
+cat.sort_values(by=['weather_date'], ascending=True, inplace=True)
 
-cat = cat.drop_duplicates().reset_index(drop=True)
+# cat = cat.drop_duplicates().reset_index(drop=True)
+
+cat.reset_index(inplace=True)
+cat['index'] = cat['index'] + 1
+cat.rename(columns={'index': 'weather_id'}, inplace=True)
 
 
 def filename():
-    return '인천공항기상현황_'+'200701'+'-'+'20240319' + '_'+ str(time.time()) + '.csv'
+    START_DATE = cat['weather_date'].min()
+    START_DATE = START_DATE[:4]+START_DATE[5:7]+START_DATE[8:10]
+    END_DATE = cat['weather_date'].max()
+    END_DATE = END_DATE[:4]+END_DATE[5:7]+END_DATE[8:10]
+
+
+    return '인천공항기상현황_' + START_DATE + '-' + END_DATE + '_' + str(time.time()) + '.csv'
 
 
 filepath = path + "/data/" + filename()
+print(filepath)
 cat.to_csv(filepath, encoding='cp949', index=False)
 print("new " + filepath + " created")
-
-
